5824924faa8d3d7dccf52a633227e95274eda8de
Pierre Marot <pierre@qualifio.com>
2017-11-08 10:05:09 +0000

    create messages

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,38 @@
-console.log('joao')
+
+interface PositionAcquired {
+  kind: 'PositionAcquired';
+  price: number;
+}
+
+interface PriceUpdated {
+  kind: 'PriceUpdated';
+  price: number;
+}
+
+interface MovingUp {
+  kind: 'MovingUp';
+  targetPrice: number;
+  timestamp: number;
+}
+
+interface SendMeIn {
+  kind: 'SendMeIn';
+  second: number;
+}
+
+interface RemoveFromTenSecondWindow {
+  kind: 'RemoveFromTenSecondWindow';
+  price: number;
+}
+
+interface RemoveFromFifteenSecondWindow {
+  kind: 'RemoveFromFifteenSecondWindow';
+  price: number;
+}
+
+type Event = SendMeIn | MovingUp | PositionAcquired;
+type Command = RemoveFromTenSecondWindow | RemoveFromFifteenSecondWindow;
+
+interface IBus {
+  publish(message: Event);
+}
